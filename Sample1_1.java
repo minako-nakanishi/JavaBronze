18d43e668d3cf0dc709e4d61414ccddf9dc8d0dc
Minako Nakanishi <n135216e@gmail.com>
2017-07-11 23:47:38 +0900

    add java file

---
+++ Sample1_1.java
@@ -0,0 +1,12 @@
+/* class宣言*/
+class Sample1_1 {
+　/* mainメソッド*/
+  public static void main(String[] args) {
+
+    System.out.println("Hello!");
+  }
+}
+
+/* 1. ソースファイルには1つ以上のclass宣言が必要
+   2. プログラムの実行はmainメソッドから実行される
+   3. 拡張子は必ず.java*/
