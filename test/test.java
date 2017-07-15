8558f63e3cef1f1769a45a39c846c5e32483191f
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-15 23:56:08 +0900

    リテラル完了

--- test/test.java
+++ test/test.java
@@ -1,7 +1,27 @@
+/** リテラル */
 class Test{
 
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println(0222);// 8進数
+        /** 整数リテラル */
+        System.out.println(398); // 10進数
+        System.out.println(0222); // 8進数(0スタート))
+        System.out.println(0xbb); // 16進数(0xスタート)
+        System.out.println(0b101); // 2進数(0bスタート)
+
+        /** 浮動小数リテラル */
+        System.out.println(19.87); //10進数の小数点有り表記
+        System.out.println(3e6); //指数を表すe(またはE)を使う
+
+        /** 文字リテラル */
+        System.out.println('田'); //1つの文字を表現
+        System.out.println('\u3012'); //Unicode(特殊文字)
+
+        /** 文字リテラル */
+        System.out.println("Hello"); //複数の文字列を扱うときは""を使用する
+
+        /** 論理値リテラル */
+        System.out.println(true); //真
+        System.out.println(false);//偽
     }
 }
