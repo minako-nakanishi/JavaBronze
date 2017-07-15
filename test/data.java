7d7e0625b30547dbe72cdc520e25f5ee665d3871
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-16 00:13:35 +0900

    データ型の勉強

---
+++ test/data.java
@@ -0,0 +1,21 @@
+
+/** Javaデータ型 */
+class Data{
+
+    public static void main(String[] args){
+        /** 8ビット(-128~127)*/
+        byte num1 = 127;
+        System.out.println(num1);
+
+        /**16ビット(-32768~32767) */
+        short num2 = 32767;
+        System.out.println(num2);
+
+    
+        /**32ビット -2147483648 ~ 2147483647 */
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(Integer.MIN_VALUE);
+        int num3 = 2147483647;
+        System.out.println(num3);
+    }
+}
