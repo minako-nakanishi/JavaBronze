63dba738631a3a09cb914656e95c6ae964024ff2
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-16 15:38:18 +0900

    基本データ型

--- test/data.java
+++ test/data.java
@@ -3,26 +3,44 @@
 class Data{
 
     public static void main(String[] args){
-        /** 8ビット(-128~127)*/
+        /** 整数 8ビット(-128~127)*/
         byte num1 = 127;
         System.out.println(num1);
 
-        /**16ビット(-32768~32767) */
+        /**整数 16ビット(-32768~32767) */
         short num2 = 32767;
         System.out.println(num2);
 
     
-        /**32ビット -2147483648 ~ 2147483647 */
+        /**整数 32ビット -2147483648 ~ 2147483647 */
         // System.out.println(Integer.MAX_VALUE);
         // System.out.println(Integer.MIN_VALUE);
         int num3 = 2147483647;
         System.out.println(num3);
 
-        /** 64ビット -9223372036854775808 ~ 9223372036854775807*/
+        /** 整数 64ビット -9223372036854775808 ~ 9223372036854775807*/
         // System.out.println(Long.MAX_VALUE);
         // System.out.println(Long.MIN_VALUE);
         long num4 = 9223372036854775807L;
         System.out.println(num4);
     
+        /** 浮動小数点 32ビット */
+        // System.out.println(Float.MAX_VALUE);
+        float num5 = 3.4028235E38F;
+        System.out.println(num5);
+
+        /** 浮動小数点 64ビット */
+        // System.out.println(Double.MAX_VALUE);
+        double num6 = 1.7976931348623157E308;
+        System.out.println(num6);
+
+        /** Unicodeで表現される１文字 */
+        char code1 = '\uFFFF';
+        System.out.println(code1);
+
+        /** 理論値 true or false */
+        boolean riron = true;
+        System.out.println(riron);
     }
+
 }
