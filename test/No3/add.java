fe4219aa72558909aaaddc98ca18da6880966348
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-18 21:41:38 +0900

    演算子

---
+++ test/No3/add.java
@@ -0,0 +1,15 @@
+class Add {
+
+    public static void main(String[] args){
+        System.out.println(10 + 3);
+        System.out.println(10 - 3);
+        System.out.println(10 * 3);
+        // 整数 / 浮動小数
+        System.out.println(10 / 3.0); // 3.3333333333333335
+        // 整数 / 整数
+        System.out.println(10 / 3); // 3
+        System.out.println(10 % 3); // 余り
+
+        
+    }
+}
---
+++ test/No3/strAdd.java
@@ -0,0 +1,30 @@
+class strAdd {
+
+    public static void main(String[] args){
+
+        String str = "Hello";
+        int a = 10;
+        int b = 20;
+
+        System.out.println(str + a); // Hello10
+        /** 
+         * ①左から加算される.
+         * ②str+aが成されるため、Hello10が生成される.
+         * ③Hello10に20が加算されるためHello1020となる.
+         */
+        System.out.println(str + a + b); // Hello1020
+        /**
+         * ①(a + b)から計算される.
+         * ②Hello + 30となる
+         * ③Hello30となる
+         */
+        System.out.println(str + (a + b));
+
+        /**
+         * ①左側から加算されるため、a+bが計算される.
+         * ②30にHelloが足される.
+         * ③30Hello
+         */
+        System.out.println(a + b + str);
+    }
+}
