dece100a5d6705b0993850508135a6922d0dc5a7
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-19 22:50:15 +0900

    単項演算子の確認

---
+++ test/No3/Plus2.java
@@ -0,0 +1,13 @@
+class Plus2{
+
+    public static void main(String[] args){
+        int a = 3;
+        int b = 6;
+
+        //System.out.println(a++ * b++); //18
+        //System.out.println(a++ * ++b); //21
+        //System.out.println(++a * b++); //24
+        System.out.println(++a * ++b); // 28
+        
+    }
+}
