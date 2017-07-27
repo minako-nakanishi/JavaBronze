c5385c1f3520e1f03352dc74d7337718026ad1a8
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-27 23:17:34 +0900

    if文

---
+++ test/No3/joken.java
@@ -0,0 +1,12 @@
+/** 条件文 */
+class Joken {
+
+    public static void main(String[] args){
+
+        int num = 8;
+        if(num < 10){
+            System.out.println("numは10未満です");
+        }
+        System.out.println("if文の処理後");
+    }
+}
---
+++ test/No3/joken2.java
@@ -0,0 +1,19 @@
+
+class Joken2{
+
+    public static void main(String[] args){
+
+        int num1 = 5;
+        if(num1 < 10){
+            System.out.println("num1は10未満");
+        }
+
+        int num2 = 20;
+        if(num2 >= 100){
+            System.out.println("num2は100以上");
+        }else {
+            System.out.println("num2は100未満");
+        }
+        System.out.println("if文終わり");
+    }
+}
