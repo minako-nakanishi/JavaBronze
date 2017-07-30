01e216d02ceb354b21caf724179e95ed462c5b78
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-07-30 23:21:39 +0900

    三項演算子問題

---
+++ test/No3/test/3-3.java
@@ -0,0 +1,12 @@
+class test3_3{
+    public static void main(String[] args){
+        /** char型は空文字を代入できない */
+        char x = 5; //char型は符号なしの整数を扱える.
+        
+        if(x == 5){
+            System.out.println("true!!");
+        }else {
+        System.out.println("false");
+        }        
+    }
+}
---
+++ test/No3/test/3-4.java
@@ -0,0 +1,11 @@
+/** 三項演算子 */
+class test3_4{
+   
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        /** trueならHello, falseならBye */
+        String str = (a > b ? "Hello!" : "Bye");
+        System.out.println(str);
+    }
+}
