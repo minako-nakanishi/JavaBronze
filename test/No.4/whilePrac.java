5587fc8a9304efae44c2bffbe717891de407687f
中西美菜子 <minako@nakanishiminakonoMacBook-Pro.local>
2017-08-07 23:42:23 +0900

    while文

---
+++ test/No.4/whilePrac.java
@@ -0,0 +1,14 @@
+class while01{
+    public static void main(String[] args){
+        // int num = 0;
+        // while(num < 5){ //numの値が5未満の間、以下を繰り返す.
+        //     System.out.println("numの値は"+num+"です");
+        //     num++;
+        // }
+        int num2 = 0;
+        while(num2 > 0){
+            System.out.println("numの値は"+num2+"です");
+            num2--;
+        }
+    }
+}
